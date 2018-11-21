a32af657e2f99e469f651c007d193ba095a93aad
Richard Pappalardo <rpappalax@gmail.com>
2018-11-21 15:15:56 -0800

    fix: X86 UI test schedule task

--- tools/taskcluster/schedule-master-build.py
+++ tools/taskcluster/schedule-master-build.py
@@ -179,8 +179,8 @@ if __name__ == "__main__":
     uiWebviewARMTestTaskId, uiWebviewARMTestTask = generate_ui_test_task( [unitTestTaskId, codeQualityTaskId], "Webview", "ARM")
     schedule_task(queue, uiWebviewARMTestTaskId, uiWebviewARMTestTask)
 
-    uiWebviewARMTestTaskId, uiWebviewARMTestTask = generate_ui_test_task([unitTestTaskId, codeQualityTaskId], "Webview", "X86")
-    schedule_task(queue, uiWebviewARMTestTaskId, uiWebviewARMTestTask)
+    uiWebviewX86TestTaskId, uiWebviewX86TestTask = generate_ui_test_task([unitTestTaskId, codeQualityTaskId], "Webview", "X86")
+    schedule_task(queue, uiWebviewX86TestTaskId, uiWebviewX86TestTask)
 
     uploadNDTaskId, uploadNDTask = upload_apk_nimbledroid_task([unitTestTaskId, codeQualityTaskId])
     schedule_task(queue, uploadNDTaskId, uploadNDTask)
